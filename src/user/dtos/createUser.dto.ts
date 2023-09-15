510ff16107868c80bb5e982b62119d09f2f060fb
Rogério Teixeira Vidal <103948082+RogerioTVidal@users.noreply.github.com>
2023-09-15 18:14:02 -0300

    CreateUserDto

---
+++ src/user/dtos/createUser.dto.ts
@@ -0,0 +1,9 @@
+export interface CreateUserDto{
+    name: string;
+    email: string;
+    phone: string;
+    nin: string; //National Idenfication Number
+    user_type: string;
+    created_at: string;
+    updated_at: string;
+}
