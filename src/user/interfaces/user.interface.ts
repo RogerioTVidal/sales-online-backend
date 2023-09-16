6a98af49326afa5ed576225c8391d3da0c05b745
Rogério Teixeira Vidal <103948082+RogerioTVidal@users.noreply.github.com>
2023-09-16 14:55:54 -0300

    Criação da interface

---
+++ src/user/interfaces/user.interface.ts
@@ -0,0 +1,10 @@
+export interface User{
+    id: number;
+    name: string;
+    email: string;
+    phone: string;
+    nin: string; //National Idenfication Number
+    user_type: string;
+    created_at: string;
+    updated_at: string;
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { User } from './interfaces/user.interface/';
+
+@Injectable()
+export class UserService {
+    private users: User[] = [];
+
+    async createUser(createUserDto: CreateUserDto): Promise<User> {
+        return {
+            ...createUserDto,
+        }
+    }
+}
