4746fc06c49edc32786434ff6b7ac367b3c6630b
Rogério Teixeira Vidal <103948082+RogerioTVidal@users.noreply.github.com>
2023-09-26 19:17:40 -0300

    Alterações de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,20 +4,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    envFilePath: ['.env.development.local'],
-  }),
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       database: process.env.DB_DATABASE,
       host: process.env.DB_HOST,
-      password: process.env.DB_PWD, 
+      password: process.env.DB_PWD,
       port: Number(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       synchronize: true,
-      entities: [`${__dirname}/**/*.entity{.js,.ts}`]
+      entities: [`${__dirname}/**/*.entity{.js,.ts}`],
     }),
-    UserModule],
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
