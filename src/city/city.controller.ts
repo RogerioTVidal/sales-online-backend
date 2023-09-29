30ce87a4fa8ee0b270be18cac38c417b4f5e3036
Rogério Teixeira Vidal <103948082+RogerioTVidal@users.noreply.github.com>
2023-09-29 13:39:47 -0300

    Add getAllCitieByState

--- src/city/city.controller.ts
+++ src/city/city.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { CityService } from './city.service';
 import { CityEntity } from './entities/city.entity';
+
 
 @Controller('city')
 export class CityController {
     constructor(
         private readonly cityService: CityService
     ) { }
+
+    @Get('/:stateId')
+    async getAllCitiesByStateId(
+        @Param('stateId') stateId: number
+        ): Promise<CityEntity[]> {
+        return this.cityService.getAllCitiesByStateId(stateId);
+    }
 
     @Get()
     async getAllCities(): Promise<CityEntity[]> {
--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -1,14 +1,26 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
+import { Cache } from 'cache-manager';
 import { CityEntity } from './entities/city.entity';
+import { Inject, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 
 @Injectable()
 export class CityService {
     constructor(
         @InjectRepository(CityEntity)
         private readonly cityRepository: Repository<CityEntity>,
+        @Inject(CACHE_MANAGER) private cacheManager: Cache,
     ) {}
+
+    async getAllCitiesByStateId(stateIdPar: number): Promise<CityEntity[]>{
+        const citiesCache: CityEntity[] = await this.cache
+        return this.cityRepository.find({
+            where: {
+                stateId: stateIdPar,
+            }
+        });
+    }
 
     async getAllcities(): Promise<CityEntity[]>{
         return this.cityRepository.find();
